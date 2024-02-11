9a623b93aa1ff90897da8126351b3cb734b9efd4
Andruleek <154084991+Andruleek@users.noreply.github.com>
2024-02-11 20:14:07 +0200

    Update go.py

--- go.py
+++ go.py
@@ -6,19 +6,20 @@ class Contact:
         self.phone = phone
 
 class AddressBook:
-    def __init__(self):
+    def __init__(self, filename):
         self.contacts = []
+        self.filename = filename
 
     def add_contact(self, contact):
         self.contacts.append(contact)
 
-    def save_to_file(self, filename):
-        with open(filename, 'wb') as file:
+    def save_to_file(self):
+        with open(self.filename, 'wb') as file:
             pickle.dump(self.contacts, file)
 
-    def load_from_file(self, filename):
+    def load_from_file(self):
         try:
-            with open(filename, 'rb') as file:
+            with open(self.filename, 'rb') as file:
                 self.contacts = pickle.load(file)
         except FileNotFoundError:
             self.contacts = []
@@ -28,27 +29,29 @@ class AddressBook:
                              if search_term.lower() in contact.name.lower() or search_term in contact.phone]
         return matching_contacts
 
+    def __enter__(self):
+        self.load_from_file()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.save_to_file()
+
 # Приклад використання
-address_book = AddressBook()
+filename = 'address_book.pickle'
+with AddressBook(filename) as address_book:
+    # Додавання контактів
+    address_book.add_contact(Contact("John Doe", "123456789"))
+    address_book.add_contact(Contact("Jane Doe", "987654321"))
+    address_book.add_contact(Contact("Taras", "087654321"))
 
-# Додавання контактів
-address_book.add_contact(Contact("John Doe", "123456789"))
-address_book.add_contact(Contact("Jane Doe", "987654321"))
-address_book.add_contact(Contact("Taras", "087654321"))
+    # Пошук контактів за ім'ям або номером телефону
+    search_term = input("Введіть ім'я або номер телефону для пошуку: ")
+    matching_contacts = address_book.search_contacts(search_term)
 
-# Збереження адресної книги на диск
-address_book.save_to_file('address_book.pickle')
+    if matching_contacts:
+        print("Знайдені контакти:")
+        for contact in matching_contacts:
+            print(f"{contact.name}: {contact.phone}")
+    else:
+        print("Контакти не знайдені.")
 
-# Завантаження адресної книги з диска
-address_book.load_from_file('address_book.pickle')
-
-# Пошук контактів за ім'ям або номером телефону
-search_term = input("Введіть ім'я або номер телефону для пошуку: ")
-matching_contacts = address_book.search_contacts(search_term)
-
-if matching_contacts:
-    print("Знайдені контакти:")
-    for contact in matching_contacts:
-        print(f"{contact.name}: {contact.phone}")
-else:
-    print("Контакти не знайдені.")
